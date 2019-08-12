764f97bd36ebdb5aef71e195c6a7890b958a6845
Navi05navi <48052925+Navi05navi@users.noreply.github.com>
2019-08-12 20:36:51 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    int low, high, i, flag;
+    int low, high, k, flag;
     printf("Enter two numbers(intervals): ");
     scanf("%d %d", &low, &high);
     printf("Prime numbers between %d and %d are: ", low, high);
     while (low < high)
     {
         flag = 0;
-        for(i = 2; i <= low/2; ++i)
+        for(k = 2; k <= low/2; ++k)
         {
-            if(low % i == 0)
+            if(low % k == 0)
             {
                 flag = 1;
                 break;
